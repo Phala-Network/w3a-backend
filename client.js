004483404d84e0602056412e07f56ed118c42d88
goldenfiredo <goldenfiredo@gmail.com>
2020-09-01 21:06:34 +0800

    main function

--- client.js
+++ client.js
@@ -1,56 +1,75 @@
 const sqlite = require('better-sqlite3');
 const { send_request } = require('./send_request');
-const { write_key, get_date_str, get_seconds_from_date_str, get_first_day_of_week } = require('./utils');
+const { write_key, get_datetime_str, get_minute_str, get_date_str, get_seconds_from_date_str, get_first_day_of_week } = require('./utils');
 
+const sleep = ms => new Promise( res => setTimeout(res, ms));
 const db = new sqlite("../db/development.sqlite3");
 
+const SET_PAGE_VIEW_KEY = "last_set_page_views";
+const ONLINE_USER_KEY = "last_processed_online_users_timestamp";
+const HOURLY_STAT_KEY = "last_processed_hourly_stats_timestamp";
+
 function set_page_views() {
-  const last_read = db.prepare("SELECT * from key_values where key = 'last_read_page_views'").all();
+  const last_read = db.prepare("SELECT * from key_values where key = ?").all(SET_PAGE_VIEW_KEY);
   let page_views
   if (last_read.length == 0) {
     page_views = db.prepare("SELECT * FROM page_views order by created_at").all();
   } else {
-    console.log(last_read[0].datetime_value);
-    page_views = db.prepare("SELECT * FROM page_views where created_at > ? order by created_at").all(last_read[0].datetime_value);
+    let now_str = get_datetime_str();
+    page_views = db.prepare("SELECT * FROM page_views where created_at >= ? and created_at < ? order by created_at").all(last_read[0].datetime_value, get_minute_str(now_str));
   }
   //console.log(page_views);
   
   let rows = [];
   for (let i in page_views) {
-    let row = page_views[i];
+    let pv = page_views[i];
 
-    let ts = Math.floor(new Date(row.created_at + 'Z').getTime() / 1000);
-    let r = {
-      "id": row.id,
-      "sid": row.sid,
-      "cid": row.cid,
-      "host": row.host,
-      "path": row.path,
-      "referrer": row.referrer,
-      "ip": row.ip,
-      "user_agent": row.ua,
-      "created_at": ts
+    let row = {
+      "id": pv.id,
+      "sid": pv.sid,
+      "cid": pv.cid,
+      "host": pv.host,
+      "path": pv.path,
+      "referrer": pv.referrer,
+      "ip": pv.ip,
+      "user_agent": pv.ua,
+      "created_at": Math.floor(new Date(pv.created_at + 'Z').getTime() / 1000)
     }
     
-    rows.push(r);
+    rows.push(row);
+  }
+
+  if (rows.length == 0) {
+    console.log("no new page view");
+    return false;
   }
 
   let payload = {
     "page_views": rows
   };
-  send_request({"SetPageView": payload});
 
-  write_key(db, 'last_read_page_views', last_read.length == 0);
+  let response = send_request({"SetPageView": payload});
+  if (response.status != "ok") {
+    console.log("response error");
+    return false;
+  }
+
+  let plain = JSON.parse(response.payload).Plain;
+  let count = JSON.parse(plain).SetPageView.page_views;
+
+  write_key(db, SET_PAGE_VIEW_KEY, last_read.length == 0);
+
+  return count > 0;
 }
 
 function update_online_users() {
-  const last_processed_online = db.prepare("SELECT * from key_values where key = ?").all('last_processed_online_users_timestamp');
+  const last_processed_online = db.prepare("SELECT * from key_values where key = ?").all(ONLINE_USER_KEY);
   let last_datetime
   if (last_processed_online.length == 0) {
     let page_views = db.prepare("SELECT * FROM page_views order by created_at").all();
     if (page_views.length > 0) {
       let created_at = page_views[0].created_at;
-      last_datetime = get_seconds_from_date_str(created_at) - 60;  
+      last_datetime = get_seconds_from_date_str(created_at);  
     }
   } else {
     last_datetime = get_seconds_from_date_str(last_processed_online[0].datetime_value);
@@ -63,7 +82,7 @@ function update_online_users() {
 
   let now = get_seconds_from_date_str();
   if (now < last_datetime + 60) {
-    console.log("too quick request");
+    console.log("update_online_users: too quick request");
     return;
   }
 
@@ -71,7 +90,7 @@ function update_online_users() {
     "start": last_datetime,
     "end": now,
   }
-  console.log('payload:', payload);
+  //console.log('payload:', payload);
 
   let response = send_request({"GetOnlineUsers": payload});
   if (response.status != "ok") {
@@ -81,20 +100,19 @@ function update_online_users() {
 
   let plain = JSON.parse(response.payload).Plain;
   let online_users = JSON.parse(plain).GetOnlineUsers.online_users;
-  console.log(online_users);
   for (let i in online_users) {
     let ou = online_users[i];
-    let d = get_date_str(ou.timestamp * 1000).substring(0, 19);
-    let now_str = get_date_str();
+    let d = get_datetime_str(ou.timestamp * 1000).substring(0, 19);
+    let now_str = get_datetime_str();
     let stmt = db.prepare("INSERT INTO online_users_reports(site_id, unique_cid_count, unique_ip_count, timestamp, date, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)");
-    stmt.run(ou.sid, ou.cid_count, ou.ip_count, d, d.split(' ')[0], now_str, now_str);
+    stmt.run(ou.sid, ou.cid_count, ou.ip_count, d, get_date_str(d), now_str, now_str);
   }
 
-  write_key(db, 'last_processed_online_users_timestamp', last_processed_online.length == 0);
+  write_key(db, ONLINE_USER_KEY, last_processed_online.length == 0);
 }
 
 function get_hourly_stats() {
-  const last_get_hourly_stats = db.prepare("SELECT * from key_values where key = ?").all('last_processed_hourly_stats_timestamp');
+  const last_get_hourly_stats = db.prepare("SELECT * from key_values where key = ?").all(HOURLY_STAT_KEY);
   //console.log(last_get_hourly_stats);
   let last_datetime
   if (last_get_hourly_stats.length == 0) {
@@ -113,7 +131,7 @@ function get_hourly_stats() {
 
   let now = get_seconds_from_date_str();
   if (Math.floor(now/3600) <= Math.floor(last_datetime/3600)) {
-    console.log("too quick request");
+    console.log("get_hourly_stats: too quick request");
     return;
   }
 
@@ -123,7 +141,7 @@ function get_hourly_stats() {
     "end": now,
     "start_of_week": start_of_week,
   }
-  console.log('payload:', payload);
+  //console.log('payload:', payload);
 
   let response = send_request({"GetHourlyStats": payload});
   if (response.status != "ok") {
@@ -135,13 +153,26 @@ function get_hourly_stats() {
   let hourly_stat = JSON.parse(plain).GetHourlyStats.hourly_stat;
   console.log('hourly_stat:', JSON.stringify(hourly_stat));
   
-  let hourly_page_views = hourly_stat.hpv;
+  process_hourly_stats(hourly_stat.hourly_page_views);
+
+  process_site_clients(hourly_stat.site_clients);
+
+  process_weekly_clients(hourly_stat.weekly_clients);
+
+  process_weekly_sites(hourly_stat.weekly_sites);
+
+  process_weekly_devices(hourly_stat.weekly_devices);
+
+  write_key(db, HOURLY_STAT_KEY, last_get_hourly_stats.length == 0);
+}
+
+function process_hourly_stats(hourly_page_views) {
   for (let i in hourly_page_views) {
     let hs = hourly_page_views[i];
-    let d = get_date_str(hs.timestamp * 1000).substring(0, 19);
-    let now_str = get_date_str();
+    let d = get_datetime_str(hs.timestamp * 1000).substring(0, 19);
+    let now_str = get_datetime_str();
     let stmt = db.prepare("INSERT INTO hourly_stats_reports(site_id, pv_count, clients_count, avg_duration_in_seconds, timestamp, date, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?, ?)");
-    stmt.run(hs.sid, hs.pv_count, hs.cid_count, hs.avg_duration, d, d.split(' ')[0], now_str, now_str);
+    stmt.run(hs.sid, hs.pv_count, hs.cid_count, hs.avg_duration, d, get_date_str(d), now_str, now_str);
 
     let count = 0;
     let result = db.prepare("SELECT * from total_stats_reports where site_id = ? and timestamp = ? order by created_at desc").all(hs.sid, d);
@@ -152,15 +183,16 @@ function get_hourly_stats() {
     stmt = db.prepare("INSERT INTO total_stats_reports(site_id, clients_count, pv_count, avg_duration_in_seconds, timestamp, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)");
     stmt.run(hs.sid, hs.cid_count, hs.pv_count + count, hs.avg_duration / 2, d, now_str, now_str);
   }
+}
 
-  let site_clients = hourly_stat.sc;
+function process_site_clients(site_clients) {
   for (let i in site_clients) {
     let sc = site_clients[i];
     let sid = sc.sid;
     for (let j in sc.cids) {
       let cid = sc.cids[j];
       let result = db.prepare("SELECT * from site_clients where site_id = ? and cid = ?").all(sid, cid);
-      let now_str = get_date_str();
+      let now_str = get_datetime_str();
       if (result.length == 0) {
         let stmt = db.prepare("INSERT INTO site_clients(site_id, cid, created_at) VALUES(?, ?, ?)");
         stmt.run(sid, cid, now_str);
@@ -173,29 +205,31 @@ function get_hourly_stats() {
       }
     }
   }
+}
 
-  let weekly_clients = hourly_stat.wc;
+function process_weekly_clients(weekly_clients) {
   for (let i in weekly_clients) {
     let wc = weekly_clients[i];
     let sid = wc.sid;
-    let date_str = get_date_str(wc.timestamp * 1000).split(' ')[0];
+    let date_str = get_date_str(get_datetime_str(wc.timestamp * 1000));
     for (let j in wc.cids) {
       let cid = wc.cids[j];
       let result = db.prepare("SELECT * from weekly_clients where site_id = ? and cid = ? and date = ?").all(sid, cid, date_str);
-      let now_str = get_date_str();
+      let now_str = get_datetime_str();
       if (result.length == 0) {
         let stmt = db.prepare("INSERT INTO weekly_clients(site_id, cid, date, created_at) VALUES(?, ?, ?, ?)");
         stmt.run(sid, cid, date_str, now_str);
       }
     }
   }
+}
 
-  let weekly_sites = hourly_stat.ws;
+function process_weekly_sites(weekly_sites) {
   for (let i in weekly_sites) {
     let ws = weekly_sites[i];
     let count = ws.count;
-    let d = get_date_str(ws.timestamp * 1000).split(' ')[0];
-    let now_str = get_date_str();
+    let d = get_date_str(get_datetime_str(ws.timestamp * 1000));
+    let now_str = get_datetime_str();
 
     let result = db.prepare("SELECT * from weekly_sites_reports where site_id = ? and path = ? and date = ?").all(ws.sid, ws.path, d);
     if (result.length > 0) {
@@ -206,13 +240,14 @@ function get_hourly_stats() {
       stmt.run(ws.sid, ws.path, count, d, now_str, now_str);
     }
   }
+}
 
-  let weekly_devices = hourly_stat.wd;
+function process_weekly_devices(weekly_devices) {
   for (let i in weekly_devices) {
     let wd = weekly_devices[i];
     let count = wd.count;
-    let d = get_date_str(wd.timestamp * 1000).split(' ')[0];
-    let now_str = get_date_str();
+    let d = get_date_str(get_datetime_str(wd.timestamp * 1000));
+    let now_str = get_datetime_str();
 
     let result = db.prepare("SELECT * from weekly_devices where site_id = ? and device = ? and date = ?").all(wd.sid, wd.device, d);
     if (result.length > 0) {
@@ -223,8 +258,6 @@ function get_hourly_stats() {
       stmt.run(wd.sid, wd.device, count, d, now_str, now_str);
     }
   }
-
-  write_key(db, 'last_processed_hourly_stats_timestamp', last_get_hourly_stats.length == 0);
 }
 
 function get_daily_stats() {
@@ -246,13 +279,13 @@ function get_daily_stats() {
 
   let today = Math.floor(new Date().getTime() / 1000 / 24 / 3600) * 24 * 3600;
   if (today <= last_date) {
-    console.log("too quick request");
+    console.log("get_daily_stats: too quick request");
     return;
   }
 
-  let last_date_str = new Date(last_date * 1000).toISOString().replace('T', ' ');
-  let today_str = new Date(today * 1000).toISOString().replace('T', ' ');
-  let result = db.prepare("SELECT * from hourly_stats_reports where date >= ? and date < ? order by date").all(last_date_str.split(' ')[0], today_str.split(' ')[0]);
+  let last_date_str = get_datetime_str(last_date * 1000);
+  let today_str = get_datetime_str(today * 1000);
+  let result = db.prepare("SELECT * from hourly_stats_reports where date >= ? and date < ? order by date").all(get_date_str(last_date_str), get_date_str(today_str));
   let stats = [];
   for (let i in result) {
     let hs = {
@@ -269,15 +302,9 @@ function get_daily_stats() {
   let payload = {
     "daily_stat": {"stats": stats}
   }
-  console.log(payload);
+  //console.log('payload:', payload);
 
   let response = send_request({"GetDailyStats": payload});
-  if (response.status != "ok") {
-    console.log("response error");
-    return;
-  }
-
-  console.log(response);
   if (response.status != "ok") {
     console.log("response error");
     return;
@@ -289,8 +316,8 @@ function get_daily_stats() {
   stats = daily_stat.stats;
   for (let i in stats) {
     let stat = stats[i];
-    let d = get_date_str(stat.timestamp * 1000).split(' ')[0];
-    let now_str = get_date_str();
+    let d = get_date_str(get_datetime_str(stat.timestamp * 1000));
+    let now_str = get_datetime_str();
     let result = db.prepare("SELECT * from daily_stats_reports where site_id = ? and date = ?").all(stat.sid, d);
     if (result.length == 0) {
       let stmt = db.prepare("INSERT INTO daily_stats_reports(site_id, pv_count, clients_count, avg_duration_in_seconds, date, created_at, updated_at) VALUES(?, ?, ?, ?, ?, ?, ?)");
@@ -320,21 +347,34 @@ function init_db() {
 
 async function main() {
   let args = process.argv.slice(2)
-  if (args.length >= 1 && args[0] == "set") {
-    set_page_views();
-    return;
-  }
-
   if (args.length >= 1 && args[0] == "init") {
     init_db();    
     return;
   }
 
-  update_online_users();
+  let hour = -1;
+  let day = -1;
+  const interval = 60;
+  while (true) {
 
-  get_hourly_stats();
+    let ret = set_page_views();
 
-  get_daily_stats();
+    if (ret)
+      update_online_users();
+
+    if (new Date().getHours() != hour) {
+      hour = new Date().getHours();
+      get_hourly_stats();
+    }
+
+    if (new Date().getDay() != day) {
+      day = new Date().getDay();
+      get_daily_stats();
+    }
+
+    console.log(`[${new Date().toLocaleString()}] wait for ${interval} seconds ...\n`);
+    await sleep(interval * 1000);
+  }
 }
 
 main().catch(console.error).finally(() => process.exit());
--- utils.js
+++ utils.js
@@ -1,19 +1,27 @@
 function write_key(db, key, update) {
-  let now_str = get_date_str();
+  let now_str = get_datetime_str();
   if (update) {
     let stmt = db.prepare("INSERT INTO key_values(key, value_type, datetime_value, created_at, updated_at) VALUES(?, 2, ?, ?, ?)");
-    stmt.run(key, now_str.substr(0, 17) + '00', now_str, now_str);
+    stmt.run(key, get_minute_str(now_str), now_str, now_str);
   } else {
     let stmt = db.prepare("UPDATE key_values set datetime_value = ?, updated_at = ? where key = ?");
-    stmt.run(now_str.substr(0, 17) + '00', now_str, key);
+    stmt.run(get_minute_str(now_str), now_str, key);
   }
 }
 
-function get_date_str(timestamp) {
+function get_datetime_str(timestamp) {
   if (timestamp == undefined) 
     return new Date().toISOString().replace('T', ' ').replace('Z', '');
   else
     return new Date(timestamp).toISOString().replace('T', ' ').replace('Z', '');
+}
+
+function get_minute_str(datetime_str) {
+  return datetime_str.substr(0, 17) + '00';
+}
+
+function get_date_str(datetime_str) {
+  return datetime_str.split(' ')[0];
 }
 
 function get_seconds_from_date_str(date_str) {
@@ -24,13 +32,14 @@ function get_seconds_from_date_str(date_str) {
 }
 
 function get_first_day_of_week(date, from_monday) {
+  let zone = new Date().getTimezoneOffset() / 60;
   let day_of_week = date.getDay();
   let first_day_of_week = new Date(date);
   let diff = day_of_week >= from_monday ? day_of_week - from_monday : 6 - day_of_week;
   first_day_of_week.setDate(date.getDate() - diff);
-  first_day_of_week.setHours(0,0,0,0);
+  first_day_of_week.setHours(0 - zone, 0, 0, 0);
 
   return first_day_of_week.getTime() / 1000;
 }
 
-module.exports = { write_key, get_date_str, get_seconds_from_date_str, get_first_day_of_week }
+module.exports = { write_key, get_datetime_str, get_minute_str, get_date_str, get_seconds_from_date_str, get_first_day_of_week }
