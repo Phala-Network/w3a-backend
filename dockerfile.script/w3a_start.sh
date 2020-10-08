98ef0b19d5f4e47b8d964eb6d13b06f96e90c924
goldenfiredo <goldenfiredo@gmail.com>
2020-10-08 10:24:53 +0800

    fix dockerfile script

--- dockerfile.script/w3a_start.sh
+++ dockerfile.script/w3a_start.sh
@@ -1,4 +1,7 @@
 cd /root/w3a-gateway
 rails s -b 0.0.0.0 -p 7000 &
 cd /root/w3a-backend/web_server
+sed 's/localhost/'$DOCKER_IP'/g' public/index.html > index.tmp && mv index.tmp public/index.html
+sed 's/localhost/'$DOCKER_IP'/g' public/page1.html > page1.tmp && mv page1.tmp public/page1.html
+sed 's/localhost/'$DOCKER_IP'/g' public/page2.html > page2.tmp && mv page2.tmp public/page2.html
 node server
